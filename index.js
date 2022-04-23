c29920fdabccf37d00e1461d2bb2f2a5643d7862
Kevin Xue <ivexnuke@gmail.com>
2022-04-23 18:37:56 -0300

    Added timer class. Added test buttons.

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 let loadEventName = "load";
+const INTERVALS = {
+  WORK: 0,
+  OVERTIME: 1,
+  BREAK: 2,
+  LONG_BREAK: 3,
+  MAX: 4
+};
 let userOptions = {};
 const DUMMY_DATA = {
   channelName: "TEST",
   maxTime: 60,
   minTime: 25,
   adaptiveBreak: true,
+  autoStart: true,
   breakFactor: 0.2,
   longBreakFactor: 0.6,
   longBreakInterval: 4,
@@ -13,28 +21,128 @@ const DUMMY_DATA = {
   startCommand: "!start",
   pauseCommand: "!pause",
   nextCommand: "!next",
-  skipCommand: "!skip",
-  resetCommand: "!reset"
+  stopCommand: "!stop"
 };
 const COMMANDS = [];
 
+class Timer{
+  constructor(totalTime, doneCallback, updateCallback){
+    this.totalTime = totalTime;
+    this.doneCallback = doneCallback;
+    this.updateCallback = updateCallback;
+    this.elapsed = 0;
+    this.startTime = 0;
+  }
+  SetTimeLimit(time){
+    this.totalTime = time;
+    if (this.timer){
+      this.Stop();
+      this.Start();
+    }
+  }
+  SetDoneCallback(callback){
+    this.doneCallback = callback;
+  }
+  SetUpdateCallback(callback){
+    this.updateCallback = callback;
+  }
+  Update(){
+    this.elapsed = Date.now() - this.startTime;
+    if (this.updateCallback){
+      this.updateCallback();
+    }
+    if (this.elapsed >= this.totalTime){
+      if (this.doneCallback){
+        this.doneCallback();
+      }
+      Stop();
+    }
+  }
+  Start(){
+    this.startTime = Date.now();
+    if (this.elapsed){ // resume logic
+      this.startTime -= this.elapsed;
+    }
+    this.elapsed = 0;
+    this.timer = setInterval(this.Update, 1000);
+  }
+  Pause(){
+    clearInterval(this.timer);
+    this.timer = null;
+  }
+  Stop(){
+    this.elapsed = 0;
+    clearInterval(this.timer);
+    this.timer = null;
+  }
+}
+
+let timer = null;
+let currInterval = INTERVALS.WORK;
+let intervalCount = 0;
+let minBar = null;
+let maxBar = null;
+let focusLabel = null;
+
+function DrawBar(timer, interval)
+{
+  let fraction = timer.elapsed / timer.totalTime;
+
+  switch(interval){
+    case INTERVALS.WORK:
+      
+      break;
+    case INTERVALS.OVERTIME:
+      break;
+    default: // break
+      break;
+  }
+}
+
 function StartTimer(){
-
-}
-function PauseTimer(){
-
-}
-function Next(){
-
-}
-function Skip(){
-
-}
-function Reset(count){
-
+  timer.Start();
 }
 
-function CreateCommands(){
+function PauseTimer()
+{
+  timer.Pause();
+}
+
+function Next()
+{
+  console.log("next");
+  switch(currInterval)
+  {
+    case INTERVALS.WORK:
+    case INTERVALS.OVERTIME:
+      intervalCount++;
+
+      if (intervalCount % userOptions.longBreakInterval == 0)
+      {
+        currInterval = INTERVALS.LONG_BREAK;
+      }
+      else
+      {
+        currInterval = INTERVALS.BREAK;
+      }
+    break;
+    default:
+      currInterval = INTERVALS.WORK;
+    break;
+  }
+  if (!userOptions.autoStart)
+  {
+    timer.Stop();
+  }
+}
+
+function Stop()
+{
+  timer.Stop();
+}
+
+function CreateCommands()
+{
   const startCommand = {};
   startCommand.condition = new RegExp("(^|\s)" + userOptions.startCommand + "($|\s)", 'i');
   startCommand.callback = StartTimer;
@@ -50,16 +158,11 @@ function CreateCommands(){
   nextCommand.callback = Next;
   COMMANDS.push(nextCommand);
 
-  const skipCommand = {};
-  skipCommand.condition = new RegExp("(^|\s)" + userOptions.skipCommand + "($|\s)", 'i');
-  skipCommand.callback = Skip;
-  COMMANDS.push(skipCommand);
-
-  const resetCommand = {};
-  resetCommand.condition = new RegExp("(^|\s)" + userOptions.resetCommand + "($|\s)", 'i');// TODO change to lookback condition
-  resetCommand.execArgs = true;
-  resetCommand.callback = Reset;
-  COMMANDS.push(resetCommand);
+  const stopCommand = {};
+  stopCommand.condition = new RegExp("(^|\s)" + userOptions.stopCommand + "($|\s)", 'i');
+  stopCommand.execArgs = true;
+  stopCommand.callback = Stop;
+  COMMANDS.push(stopCommand);
 }
 
 window.addEventListener(loadEventName, function (obj) 
@@ -74,6 +177,13 @@ window.addEventListener(loadEventName, function (obj)
   {
     userOptions = DUMMY_DATA;
   }
+
+  timer = new Timer(userOptions.minTime, Next, function(){
+    DrawBar(timer, currInterval);
+  });
+  minBar = document.getElementById("min-bar");
+  maxBar = document.getElementById("max-bar");
+  focusLabel = document.getElementById("focus-label");
 });
 
 function IsModMessage(obj)
